38244d32e6b02ba85fe5a21089219407eaff45ca
Kavindu Lakshitha <kavindu.lakshitha.ofc@gmail.com>
2025-02-18 13:15:02 +0530

    Minor changes

--- app/Filament/Resources/SettingsResource.php
+++ app/Filament/Resources/SettingsResource.php
@@ -17,7 +17,9 @@ class SettingsResource extends Resource
 {
     protected static ?string $model = Setting::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-wrench-screwdriver';
+
+    protected static ?string $navigationGroup = 'Company Settings';
 
     public static function form(Form $form): Form
     {
