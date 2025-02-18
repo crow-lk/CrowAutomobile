2e96c1d399cc3628a2de3f4324da51cbccde8818
Kavindu Lakshitha <kavindu.lakshitha.ofc@gmail.com>
2025-02-18 11:25:49 +0530

    Company settings option added. Need to check & fine tune.

---
+++ app/Filament/Resources/SettingsResource.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SettingsResource\Pages;
+use App\Filament\Resources\SettingsResource\RelationManagers;
+use App\Models\Setting;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SettingsResource extends Resource
+{
+    protected static ?string $model = Setting::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([Forms\Components\TextInput::make('name')->required(), 
+        Forms\Components\FileUpload::make('logo')->required(), 
+        Forms\Components\TextInput::make('phone')->required(), 
+        Forms\Components\TextInput::make('email')->required()]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([Tables\Columns\TextColumn::make('name')->sortable()->searchable(), Tables\Columns\TextColumn::make('logo')->sortable()->searchable(), Tables\Columns\TextColumn::make('phone')->sortable()->searchable(), Tables\Columns\TextColumn::make('email')->sortable()->searchable()])
+            ->filters([
+                //
+            ])
+            ->actions([Tables\Actions\ViewAction::make(), Tables\Actions\EditAction::make()])
+            ->bulkActions([Tables\Actions\BulkActionGroup::make([Tables\Actions\DeleteBulkAction::make()])]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSettings::route('/'),
+        ];
+    }
+}
